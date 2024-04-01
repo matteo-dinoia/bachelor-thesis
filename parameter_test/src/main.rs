5fbb7a3caf9a1aab33b09f81a4a7e2f7812d1c1d
Matteo Di Noia <dmatteo002@gmail.com>
2024-04-01 10:01:45 +0200

    adding a more functional version of parameter changer

--- parameter_test/src/main.rs
+++ parameter_test/src/main.rs
@@ -1,11 +1,6 @@
-use std::{io::{self, Write}, mem::MaybeUninit};
-
+use std::{io::{self, Write}, mem::MaybeUninit, process::{Command, Stdio}};
 use libc::{sched_getscheduler, sched_setscheduler, sched_getparam, sched_param};
 
-
-/*extern "C" {
-    fn sched_getscheduler(pid: c_int) -> c_int;
-}*/
 
 fn get_priority(policy: i32) -> String{
     return match policy {
@@ -23,11 +18,13 @@ fn get_priority(policy: i32) -> String{
 }
 
 fn read_number(query : &str) -> i32{
-    let mut number_str = String::new();
+    let mut number_str;
 
     loop{
         print!("{}", query);
         io::stdout().flush().unwrap();
+
+        number_str = String::new();
         io::stdin().read_line(&mut number_str).unwrap();
 
         if let Ok(pid) = number_str.trim().parse::<i32>(){
@@ -38,26 +35,39 @@ fn read_number(query : &str) -> i32{
     
 }
 
-fn set_method(){
-    let pid = read_number("Insert pid on which to write: ");
-    let policy = read_number("Insert policy to change to: ");
+fn set_method(pid : i32, policy : i32){
     let mut par = MaybeUninit::<sched_param>::uninit();
 
     unsafe { sched_getparam(pid,  par.as_mut_ptr()) };
 
     let res = unsafe{sched_setscheduler(pid, policy, par.as_ptr())};
     if res == 0 {
-        println!("Successfully changed to {}", get_priority(policy));
+        println!("Successfully changed pid {} to {}", pid, get_priority(policy));
     }else{
-        println!("Failed to change to {}", get_priority(policy));
+        println!("Failed to change pid {} to {}", pid, get_priority(policy));
     }
 }
 
-fn get_method(){
-    let pid = read_number("Insert pid on which to read: ");
-    
+fn get_method(pid : i32){
     let policy = unsafe { sched_getscheduler(pid) };
-    println!("Scheduler policy for process {}: {} ({})", pid, get_priority(policy), policy);
+    println!("Scheduler policy for process {}: {} ({})", 
+            pid, get_priority(policy), policy);
+}
+
+fn list_on_shed_ext() -> String{
+    let ps_child = Command::new("ps")
+        .args(&["-Ao", "pid,class,cmd"])
+        .stdout(Stdio::piped())
+        .spawn()  
+        .unwrap();
+    let grep_child = Command::new("grep")
+        .arg("[#]7")
+        .stdin(Stdio::from(ps_child.stdout.unwrap())) // Pipe through.
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+    let output = grep_child.wait_with_output().unwrap();
+    return String::from_utf8(output.stdout).unwrap();
 }
 
 fn main() {
@@ -71,9 +81,37 @@ fn main() {
         io::stdin().read_line(&mut command).unwrap();
 
         match command.trim().to_lowercase().as_str() {
-            "r" => get_method(),
-            "" => get_method(),
-            "w" => set_method(),
+            "r" | "" => {
+                let pid = read_number("Insert pid on which to write: ");
+                get_method(pid);
+            },
+            "w" => {
+                let pid = read_number("Insert pid on which to write: ");
+                let policy = read_number("Insert policy to change to: ");
+                set_method(pid, policy);
+            },
+            "x" => {
+                let list =  list_on_shed_ext();
+                if list.lines().count() != 0 {
+                    print!("Process on scheduling class ext (#7) are:\n {}", 
+                        list);
+                } else {
+                    println!("No process on scheduling class ext (#7)");
+                }
+                
+            },
+            "y" => {
+                println!("Resetting processes to normal classes");
+
+                let list =  list_on_shed_ext();
+                let array = list.lines().map(|line | {line.trim().split(" ").next()});
+                array.for_each(|pid_str| {
+                    let pid_res = pid_str.unwrap().trim().parse::<i32>();
+                    if let Ok(pid) = pid_res{
+                        set_method(pid, 0);
+                    }
+                });
+            },
             _   => break
         };
     }
@@ -89,5 +127,3 @@ fn main() {
 	 * sched_ext. If clear, SCHED_NORMAL tasks are also included.
 */
 //SCX_OPS_SWITCH_PARTIAL	= 1LLU << 0,
-
-// TODO i need to add the option to a scheduler and then to decide to move pid there

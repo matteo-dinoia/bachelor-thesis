70f22b868354d2751b3563262bd3c0031e0e1a32
Matteo Di Noia <dmatteo002@gmail.com>
2024-04-22 17:33:03 +0200

    adding double time mode and trace

--- workload/0-loop/double.sh
+++ workload/0-loop/double.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
+# require BC
 
 # c for children
-# sudo trace-cmd record -e sched -c -F ./double.sh 1000
+# sudo trace-cmd record -e sched -c -F ./double.sh TIME [INTERVAL] 2> /dev/null
 
-pid1=0
-pid2=0
+TIME=$1 #in mills
+TIME_INTERVAL=$2 #in mills to wait before changing
 
-TIME=$1
 
-nice -n 19 ./a.out $TIME & #low time and priority
-pid1=$!
-nice -n -20 ./b.out $TIME & # high time and priority
-pid2=$!
+if test -z "$TIME_INTERVAL"; then
+	nice -n 19 ./a.out $TIME & #low time and priority
+	pid1=$!
+	nice -n -20 ./b.out $TIME & # high time and priority
+	pid2=$!
+	echo "STARTED NORMAL MODE. With pids: $pid1 $pid2"
+else
+	nice -n 19 ./a.out $TIME & #low time and priority
+	pid1=$!
+	nice -n 19 ./b.out $TIME & #low time and priority
+	pid2=$!
+	echo "STARTED TWO TIME MODE. With pids: $pid1 $pid2"
 
-echo "STARTED. With pids: $pid1 $pid2"
+	# wait for interval
+	TIME_SEC=$(echo "scale=2; $TIME_INTERVAL/1000" | bc)
+	sleep $TIME_SEC
+
+	# change both to high time and priority
+	renice -20 -p $pid1
+	renice -20 -p $pid2
+fi
 
 wait $pid1 || echo "error on pid1"
 wait $pid2 || echo "error on pid2"

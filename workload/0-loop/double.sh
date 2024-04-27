9cd2c9783ea76aec99bfd6fd00eb3d8c71ba7671
Matteo Di Noia <dmatteo002@gmail.com>
2024-04-27 21:34:30 +0200

    made it runnable without sudo

--- workload/0-loop/double.sh
+++ workload/0-loop/double.sh
@@ -3,28 +3,36 @@
 # require BC
 
 # c for children
-# sudo trace-cmd record -e sched -c -F ./double.sh TIME [INTERVAL] 2> /dev/null
+# sudo trace-cmd record -e sched -c -F ./double.sh TOTAL_MS [INBETWEEN_MS] 2> /dev/null
 
-TIME=$1 #in mills
-TIME_INTERVAL=$2 #in mills to wait before changing
+# Times in mills
+TIME_TOTAL=$1 #in mills
+TIME_INBETWEEN=$2 #in mills to wait before changing
+
+if [ "$TIME_INBETWEEN" -gt "$TIME_TOTAL" ]; then
+	echo "Total time must be larger than interval before signal"
+	exit
+fi
 
 
-if test -z "$TIME_INTERVAL"; then
-	nice -n 19 ./a.out $TIME & #low time and priority
+# Can put to 19 but not to -20 (increase but not decrease)
+# I need to swap
+if test -z "$TIME_INBETWEEN"; then
+	nice -n 19 ./a.out $TIME_TOTAL & #low time and priority
 	pid1=$!
-	nice -n -20 ./b.out $TIME & # high time and priority
+	./b.out $TIME_TOTAL & # high time and priority (priority 0)
 	pid2=$!
 	echo "STARTED NORMAL MODE. With pids: $pid1 $pid2"
 else
-	nice -n 19 ./a.out $TIME & #low time and priority
+	./a.out $TIME_TOTAL & #low time and priority
 	pid1=$!
-	nice -n 19 ./b.out $TIME & #low time and priority
+	./b.out $TIME_TOTAL & #low time and priority
 	pid2=$!
 	echo "STARTED TWO TIME MODE. With pids: $pid1 $pid2"
 
 	# wait for interval
-	TIME_SEC=$(echo "scale=2; $TIME_INTERVAL/1000" | bc)
-	sleep $TIME_SEC
+	SEC_INBETWEEN=$(echo "scale=2; $TIME_INBETWEEN/1000" | bc)
+	sleep $SEC_INBETWEEN
 
 	# change both to high time and priority
 	renice -20 -p $pid1

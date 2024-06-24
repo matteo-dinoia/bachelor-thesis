b36d0f78421ea6c08fcb482f3769595ea0bc273e
Matteo Di Noia <dmatteo002@gmail.com>
2024-06-24 15:54:52 +0200

    cleaning code of par_changer

--- parameter_changer/src/main.rs
+++ parameter_changer/src/main.rs
@@ -1,14 +1,13 @@
 use std::{io::{self, Write}, mem::MaybeUninit, process::{Command, Stdio}};
 use libc::{sched_getscheduler, sched_setscheduler, sched_getparam, sched_param};
 
-
 fn get_priority(policy: i32) -> String{
 	return match policy {
 		0  => "SCHED_NORMAL",
 		1  => "SCHED_FIFO",
 		2  => "SCHED_RR",
 		3  => "SCHED_BATCH",
-		/* "SCHED_ISO": reserved but not implemented yet */
+		/* 4 => "SCHED_ISO": reserved but not implemented yet */
 		5  => "SCHED_IDLE",
 		6  => "SCHED_DEADLINE",
 		7  => "SCHED_EXT",
@@ -18,24 +17,22 @@ fn get_priority(policy: i32) -> String{
 }
 
 fn read_number(query : &str) -> i32{
-	let mut number_str;
+	let mut number_str = String::new();
 
 	loop{
-		print!("{}", query);
-		io::stdout().flush().unwrap();
+		print!("{query}: ");
+		io::stdout().flush().unwrap_or(());
 
-		number_str = String::new();
+		number_str.clear();
 		io::stdin().read_line(&mut number_str).unwrap();
 
 		if let Ok(pid) = number_str.trim().parse::<i32>(){
 			return pid;
 		}
 	}
-	
-	
 }
 
-fn set_method(pid : i32, policy : i32){
+fn set_sched_class(pid : i32, policy : i32){
 	let mut par = MaybeUninit::<sched_param>::uninit();
 
 	unsafe { sched_getparam(pid,  par.as_mut_ptr()) };
@@ -48,24 +45,20 @@ fn set_method(pid : i32, policy : i32){
 	}
 }
 
-fn get_method(pid : i32){
+fn print_sched_class(pid : i32){
 	let policy = unsafe { sched_getscheduler(pid) };
 	println!("Scheduler policy for process {}: {} ({})", 
 			pid, get_priority(policy), policy);
 }
 
-fn get_list_in_sheduling_class(sched_class : &str) -> String{
+fn get_list_str_by_sched_class(sched_class : &str) -> String{
 	let ps_child = Command::new("ps")
 		.args(&["-Ao", "pid,class,cmd"])
-		.stdout(Stdio::piped())
-		.spawn()  
-		.unwrap();
+		.stdout(Stdio::piped()).spawn()  .unwrap();
 	let grep_child = Command::new("grep")
 		.arg(sched_class)
 		.stdin(Stdio::from(ps_child.stdout.unwrap())) // Pipe through.
-		.stdout(Stdio::piped())
-		.spawn()
-		.unwrap();
+		.stdout(Stdio::piped()).spawn().unwrap();
 	let output = grep_child.wait_with_output().unwrap();
 	return String::from_utf8(output.stdout).unwrap();
 }
@@ -74,42 +67,39 @@ fn main() {
 	let mut command = String::new();
 	
 	loop{
-		print!("\nInsert what to do (R/w/l/n/q or h for help]): ");
-		io::stdout().flush().unwrap();
+		print!("\nInsert what to do (read/write/list_sched_ext/reset_to_norm): ");
+		io::stdout().flush().unwrap_or(());
 		
 		command.clear();
 		io::stdin().read_line(&mut command).unwrap();
 
 		match command.trim().to_lowercase().as_str() {
-			"r" | "" => {
-				let pid = read_number("Insert pid on which to write: ");
-				get_method(pid);
+			"read" => {
+				let pid = read_number("Insert pid on which to write");
+				print_sched_class(pid);
 			},
-			"w" => {
-				let pid = read_number("Insert pid on which to write: ");
-				let policy = read_number("Insert policy to change to: ");
-				set_method(pid, policy);
+			"write" => {
+				let pid = read_number("Insert pid on which to write");
+				let policy = read_number("Insert policy to change to");
+				set_sched_class(pid, policy);
 			},
-			"l" => {
-				let list =  get_list_in_sheduling_class("#7");
+			"list_sched_ext" => {
+				let list =  get_list_str_by_sched_class("#7");
 				if list.lines().count() != 0 {
-					print!("Process on scheduling class ext (#7) are:\n {}", 
-						list);
+					print!("Process on scheduling class ext (#7) are:\n {list}");
 				} else {
 					println!("No process on scheduling class ext (#7)");
 				}
-				
 			},
-			"n" => {
+			"reset_to_norm" => {
 				println!("Resetting processes to normal classes");
 
-				let list =  get_list_in_sheduling_class("#7");
-				let array = list.lines().map(|line | {line.trim().split(" ").next()});
-				array.for_each(|pid_str| {
-					let pid_res = pid_str.unwrap().trim().parse::<i32>();
-					if let Ok(pid) = pid_res{
-						set_method(pid, 0);
-					}
+				let list_str =  get_list_str_by_sched_class("#7");
+				let iterator = list_str.lines().map(|line| {line.trim().split(" ").next().unwrap()});
+				
+				iterator.for_each(|pid_str| {
+					let pid = pid_str.parse::<i32>().unwrap();
+					set_sched_class(pid, 0);
 				});
 			},
 			"h" => {
@@ -123,7 +113,7 @@ fn main() {
 				println!("  q\tQuit");
 				println!("--------------------------------------------------");
 				println!();
-			}
+			},
 			_   => break
 		};
 	}

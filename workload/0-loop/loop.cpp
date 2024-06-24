648369768c22d38e4667ba7ef71b8ad4bcd3b678
Matteo Di Noia <dmatteo002@gmail.com>
2024-06-24 16:26:35 +0200

    clean loop.c

--- workload/0-loop/loop.cpp
+++ workload/0-loop/loop.cpp
@@ -1,7 +1,3 @@
-#ifndef _GNU_SOURCE
-#define _GNU_SOURCE
-#endif
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -9,6 +5,8 @@
 #include <sys/time.h>
 #include <sched.h>
 
+#define ASSERT(cond, err_str) \
+	if((cond)){ perror((err_str)); exit(1); }
 
 volatile bool running;
 volatile long long int first_half_work = -1;
@@ -18,23 +16,18 @@ void handle_signal(int sig);
 
 
 void setup_shed_param(){
-	// Setting itself in the sched_ext class (0 = itself, 7 = sched_ext)
+	// Setting itself (0) in the sched_ext class (7)
 	struct sched_param par;
 	sched_getparam(0,  &par);
-	int res = sched_setscheduler(0, 7, &par);
-	if (res != 0) {
-		perror("sched_setscheduler");
-		exit(1);
-	}
+	const int ret1 = sched_setscheduler(0, 7, &par);
+	ASSERT(ret1 != -1, "sched_setscheduler");
 
 	// Setting itself on last cpu (15)
 	cpu_set_t mask;
 	CPU_ZERO(&mask);
 	CPU_SET(15, &mask);
-	if (sched_setaffinity(0, sizeof(mask), &mask) == -1) {
-		perror("cpuaffinity");
-		exit(1);
-	}
+	const int ret2 = sched_setaffinity(0, sizeof(mask), &mask);
+	ASSERT(ret2 != -1, "cpuaffinity");
 }
 
 void start_timer(int  number_millisec){
@@ -45,12 +38,9 @@ void start_timer(int  number_millisec){
 	sa.sa_handler = &handle_signal;
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
-	const int res1 = sigaction(SIGALRM, &sa, NULL);
-	const int res2 = sigaction(SIGUSR1, &sa, NULL);
-	if (res1 == -1 || res2 == -1) {
-		perror("sigaction");
-		exit(1);
-	}
+	const int ret1 = sigaction(SIGALRM, &sa, NULL);
+	const int ret2 = sigaction(SIGUSR1, &sa, NULL);
+	ASSERT((ret1 == -1 || ret2 == -1), "sigaction");
 
 	// Setup timer
 	int number_sec = number_millisec / 1000;
@@ -60,43 +50,32 @@ void start_timer(int  number_millisec){
 	timer.it_interval.tv_sec = 0;
 	timer.it_interval.tv_usec = 0;
 
-	if (setitimer(ITIMER_REAL, &timer, NULL) == -1) {
-		perror("setitimer");
-		exit(1);
-	}
+	const int ret3 = setitimer(ITIMER_REAL, &timer, NULL);
+	ASSERT(ret3 != -1, "cpuaffinity");
 }
 
 int get_ms(int argc, char **argv){
 	// Get timer value
-	if (argc != 2) {
-		printf("Usage: %s <number_millisec>\n", argv[0]);
-		return -1;
-	}
+	ASSERT(argc != 2, "Usage: program_name <ms_duration>");
 
 	// Convert the string argument to a number
-	int number_millisec = atoi(argv[1]);
+	int millisec;
+	const int ret = sscanf(argv[1], "%d", &millisec);
+	ASSERT((ret == 1 && millisec > 0), 
+			"First parameter should be a positive integer");
 
-	// Check if conversion was successful
-	if (number_millisec <= 0 && argv[1][0] != '0') {
-		printf("Invalid number: %s\n", argv[1]);
-		return -1;
-	}
-
-	return number_millisec;
+	return millisec;
 }
 
 void handle_signal(int sig) {
-	if(sig == SIGUSR1){
+	if(sig == SIGUSR1)
 		first_half_work = counter;
-	}else if (sig == SIGALRM){
+	else if (sig == SIGALRM)
 		running = false;
-	}
 }
 
 int main(int argc, char **argv) {
 	int ms = get_ms(argc, argv);
-	if(ms == -1)
-		return -1;
 
 	setup_shed_param();
 	start_timer(ms);
@@ -108,13 +87,9 @@ int main(int argc, char **argv) {
 
 	// Print result
 	printf("File %s made: %lld", argv[0], counter);
-	if(first_half_work != -1){
+	if(first_half_work != -1){ //if was split in two times
 		printf(" (%lld before and %lld after)",
 			   first_half_work, (counter - first_half_work));
 	}
 	printf("\n");
-
-
-
-	return 0;
 }
